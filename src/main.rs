d4e41eb8ea3d215466108d9c00218d8eecf1abc5
Kiyotoko <karl.zschiebsch@gmail.com>
2025-09-29 20:11:42 +0200

    feat: add mail

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 mod player;
 mod shaders;
 
-use crate::player::Player;
+use crate::player::{Mail, Player};
 use crate::shaders::{CRT_FRAGMENT_SHADER, CRT_VERTEX_SHADER};
 use macroquad::prelude::*;
-use macroquad::ui::widgets::Slider;
 use macroquad::ui::{
-    Drag, Ui, hash, root_ui,
-    widgets::{self, Group},
+    Skin, hash, root_ui,
+    widgets::{Group, Window},
 };
 
 fn window_conf() -> Conf {
@@ -19,147 +18,8 @@ fn window_conf() -> Conf {
     }
 }
 
-pub struct Slot {
-    id: u64,
-    item: Option<String>,
-}
-impl Slot {
-    fn new(id: u64) -> Slot {
-        Slot { id, item: None }
-    }
-}
-
-pub enum FittingCommand {
-    /// Remove item from this slot
-    Unfit { target_slot: u64 },
-    /// Fit item from inventory to slot
-    Fit { target_slot: u64, item: String },
-    /// Move item from one slot to another
-    Refit { target_slot: u64, origin_slot: u64 },
-}
-
-pub struct Data {
-    inventory: Vec<String>,
-    item_dragging: bool,
-    slots: Vec<(&'static str, Slot)>,
-    fit_command: Option<FittingCommand>,
-}
-impl Data {
-    pub fn new() -> Data {
-        Data {
-            inventory: vec![],
-            item_dragging: false,
-            slots: vec![
-                ("Left Mouse Button", Slot::new(hash!())),
-                ("Right Mouse Button", Slot::new(hash!())),
-                ("Middle Mouse Button", Slot::new(hash!())),
-                ("Space", Slot::new(hash!())),
-                ("\"1\"", Slot::new(hash!())),
-                ("\"2\"", Slot::new(hash!())),
-                ("\"3\"", Slot::new(hash!())),
-            ],
-            fit_command: None,
-        }
-    }
-
-    fn slots(&mut self, ui: &mut Ui) {
-        let item_dragging = &mut self.item_dragging;
-
-        let fit_command = &mut self.fit_command;
-        for (label, slot) in self.slots.iter_mut() {
-            Group::new(hash!("grp", slot.id, &label), Vec2::new(210., 55.)).ui(ui, |ui| {
-                let drag = Group::new(slot.id, Vec2::new(50., 50.))
-                    // slot without item is not draggable
-                    .draggable(slot.item.is_some())
-                    // but could be a target of drag
-                    .hoverable(*item_dragging)
-                    // and is highlighted with other color when some item is dragging
-                    .highlight(*item_dragging)
-                    .ui(ui, |ui| {
-                        if let Some(ref item) = slot.item {
-                            ui.label(Vec2::new(5., 10.), &item);
-                        }
-                    });
-
-                match drag {
-                    // there is some item in this slot and it was dragged to another slot
-                    Drag::Dropped(_, Some(id)) if slot.item.is_some() => {
-                        *fit_command = Some(FittingCommand::Refit {
-                            target_slot: id,
-                            origin_slot: slot.id,
-                        });
-                    }
-                    // there is some item in this slot and it was dragged out - unfit it
-                    Drag::Dropped(_, None) if slot.item.is_some() => {
-                        *fit_command = Some(FittingCommand::Unfit {
-                            target_slot: slot.id,
-                        });
-                    }
-                    // there is no item in this slot
-                    // this is impossible - slots without items are non-draggable
-                    Drag::Dropped(_, _) => unreachable!(),
-                    Drag::Dragging(pos, id) => {
-                        debug!("slots: pos: {:?}, id {:?}", pos, id);
-                        *item_dragging = true;
-                    }
-                    Drag::No => {}
-                }
-                ui.label(Vec2::new(60., 20.), label);
-            });
-        }
-    }
-
-    fn inventory(&mut self, ui: &mut Ui) {
-        let item_dragging = &mut self.item_dragging;
-        for (n, item) in self.inventory.iter().enumerate() {
-            let drag = Group::new(hash!("inventory", n), Vec2::new(50., 50.))
-                .draggable(true)
-                .ui(ui, |ui| {
-                    ui.label(Vec2::new(5., 10.), &item);
-                });
-
-            match drag {
-                Drag::Dropped(_, Some(id)) => {
-                    self.fit_command = Some(FittingCommand::Fit {
-                        target_slot: id,
-                        item: item.clone(),
-                    });
-                    *item_dragging = false;
-                }
-                Drag::Dropped(_, _) => {
-                    *item_dragging = false;
-                }
-                Drag::Dragging(pos, id) => {
-                    debug!("inventory: pos: {:?}, id {:?}", pos, id);
-                    *item_dragging = true;
-                }
-                _ => {}
-            }
-        }
-    }
-
-    fn set_item(&mut self, id: u64, item: Option<String>) {
-        if let Some(slot) = self.slots.iter_mut().find(|(_, slot)| slot.id == id) {
-            slot.1.item = item;
-        }
-    }
-}
-
 #[macroquad::main(window_conf)]
 async fn main() {
-    let mut data = Data::new();
-
-    let mut data0 = String::new();
-    let mut data1 = String::new();
-
-    let mut text0 = String::new();
-    let mut text1 = String::new();
-
-    let mut number0 = 0.;
-    let mut number1 = 0.;
-    let mut number2 = 0.;
-
-    let texture: Texture2D = load_texture("assets/ferris.png").await.unwrap();
     let render_target = render_target(320, 150);
     render_target.texture.set_filter(FilterMode::Nearest);
 
@@ -171,6 +31,29 @@ async fn main() {
         Default::default(),
     )
     .unwrap();
+
+    let skin = {
+        let label_style = root_ui()
+            .style_builder()
+            .font_size(18)
+            .margin(RectOffset::new(2., 2., 2., 2.))
+            .build();
+
+        Skin {
+            label_style,
+            ..root_ui().default_skin()
+        }
+    };
+
+    let mut player = Player::new();
+    player.add_mail(Mail::new(
+        "Teewurst Inc.".into(),
+        "Welcome to Teewurst Inc.".into(),
+    ));
+    player.add_mail(Mail::new(
+        "Teewurst Inc.".into(),
+        "You are the new AI 9000. Your job is to\ncontrol the car market. Make no mistakes.".into(),
+    ));
 
     loop {
         #[cfg(not(target_arch = "wasm32"))]
@@ -184,117 +67,36 @@ async fn main() {
         });
         clear_background(WHITE);
 
-        widgets::Window::new(hash!(), vec2(400., 200.), vec2(320., 400.))
+        Window::new(hash!(), vec2(400., 50.), vec2(320., 400.))
             .label("Shop")
             .titlebar(true)
             .ui(&mut *root_ui(), |ui| {
-                for i in 0..30 {
+                for i in 0..10 {
                     Group::new(hash!("shop", i), Vec2::new(300., 80.)).ui(ui, |ui| {
-                        ui.label(Vec2::new(10., 10.), &format!("Item N {i}"));
+                        ui.label(Vec2::new(10., 10.), &format!("Item N {}", i));
                         ui.label(Vec2::new(260., 40.), "10/10");
                         ui.label(Vec2::new(200., 58.), &format!("{} kr", 800));
-                        if ui.button(Vec2::new(260., 55.), "buy") {
-                            data.inventory.push(format!("Item {i}"));
+                        if ui.button(Vec2::new(260., 55.), "buy") && player.can_buy(800.) {
+                            dbg!("buyed item");
                         }
                     });
                 }
             });
 
-        widgets::Window::new(hash!(), vec2(100., 220.), vec2(542., 430.))
-            .label("Fitting window")
-            .titlebar(true)
+        Window::new(hash!(), Vec2::new(50., 50.), vec2(300., 500.))
+            .label("E-Mails")
             .ui(&mut *root_ui(), |ui| {
-                Group::new(hash!(), Vec2::new(230., 400.)).ui(ui, |ui| {
-                    data.slots(ui);
-                });
-                Group::new(hash!(), Vec2::new(280., 400.)).ui(ui, |ui| {
-                    data.inventory(ui);
-                });
-            });
-
-        widgets::Window::new(hash!(), vec2(470., 50.), vec2(300., 300.))
-            .label("Megaui Showcase Window")
-            .ui(&mut *root_ui(), |ui| {
-                ui.tree_node(hash!(), "input", |ui| {
-                    ui.label(None, "Some random text");
-                    if ui.button(None, "click me") {
-                        println!("hi");
-                    }
-
-                    ui.separator();
-
-                    ui.label(None, "Some other random text");
-                    if ui.button(None, "other button") {
-                        println!("hi2");
-                    }
-
-                    ui.separator();
-
-                    ui.input_text(hash!(), "<- input text 1", &mut data0);
-                    ui.input_text(hash!(), "<- input text 2", &mut data1);
-                    ui.label(None, &format!("Text entered: \"{data0}\" and \"{data1}\""));
-
-                    ui.separator();
-                });
-                ui.tree_node(hash!(), "buttons", |ui| {
-                    widgets::Button::new(texture.clone())
-                        .size(vec2(120., 70.))
-                        .ui(ui);
-                    ui.same_line(0.);
-                    widgets::Button::new("Button").size(vec2(120., 70.)).ui(ui);
-                    widgets::Button::new("Button").size(vec2(120., 70.)).ui(ui);
-                    ui.same_line(0.);
-                    widgets::Button::new(texture.clone())
-                        .size(vec2(120., 70.))
-                        .ui(ui);
-                });
-                ui.tree_node(hash!(), "sliders and bars", |ui| {
-                    let range0 = -10f32..10f32;
-                    ui.slider(hash!(), "[-10 .. 10]", range0.clone(), &mut number0);
-
-                    let range1 = 0f32..100f32;
-                    ui.slider(hash!(), "[0 .. 100]", range1.clone(), &mut number1);
-
-                    let range2 = 0f32..1f32;
-                    Slider::new(hash!(), range2.clone())
-                        .label("slider with a long label")
-                        .ui(ui, &mut number2);
-                });
-                ui.tree_node(hash!(), "editbox 1", |ui| {
-                    ui.label(None, "This is editbox!");
-                    ui.editbox(hash!(), vec2(285., 165.), &mut text0);
-                });
-                ui.tree_node(hash!(), "editbox 2", |ui| {
-                    ui.label(None, "This is editbox!");
-                    ui.editbox(hash!(), vec2(285., 165.), &mut text1);
-                });
-            });
-
-        match data.fit_command.take() {
-            Some(FittingCommand::Unfit { target_slot }) => data.set_item(target_slot, None),
-            Some(FittingCommand::Fit { target_slot, item }) => {
-                data.set_item(target_slot, Some(item));
-            }
-            Some(FittingCommand::Refit {
-                target_slot,
-                origin_slot,
-            }) => {
-                let origin_item = data
-                    .slots
-                    .iter()
-                    .find_map(|(_, slot)| {
-                        if slot.id == origin_slot {
-                            Some(slot.item.clone())
-                        } else {
-                            None
+                for mail in player.fetch_mails() {
+                    Group::new(hash!(mail.get_message()), Vec2::new(292., 80.)).ui(ui, |ui| {
+                        ui.push_skin(&skin);
+                        ui.label(None, &format!("from: {}", mail.get_author()));
+                        ui.pop_skin();
+                        for line in mail.get_message().split('\n') {
+                            ui.label(None, line);
                         }
-                    })
-                    .flatten();
-                data.set_item(target_slot, origin_item);
-                data.set_item(origin_slot, None);
-            }
-            None => {}
-        };
+                    });
+                }
+            });
 
         set_default_camera();
         clear_background(WHITE);
@@ -305,7 +107,7 @@ async fn main() {
             0.,
             WHITE,
             DrawTextureParams {
-                dest_size: Some(vec2(screen_width(), screen_height())),
+                dest_size: Some(Vec2::new(screen_width(), screen_height())),
                 ..Default::default()
             },
         );
--- src/player.rs
+++ src/player.rs
@@ -1,14 +1,58 @@
 use macroquad::prelude::*;
 
-const PLAYER_SPEED: f32 = 6.15;
+#[derive(Debug)]
+pub struct Mail {
+    pub id: usize,
+    pub author: String,
+    pub message: String,
+}
+
+impl Mail {
+    pub fn new(author: String, message: String) -> Mail {
+        Mail {
+            id: 0,
+            author,
+            message,
+        }
+    }
+
+    pub fn get_author(&self) -> &String {
+        &self.author
+    }
+
+    pub fn get_message(&self) -> &String {
+        &self.message
+    }
+}
 
 #[derive(Debug)]
 pub struct Player {
+    mails: Vec<Mail>,
     money: f64,
 }
 
 impl Player {
     pub fn new() -> Player {
-        Player { money: 0. }
+        Player {
+            mails: Vec::new(),
+            money: 0.,
+        }
+    }
+
+    pub fn income(&mut self, value: f64) {
+        self.money += value;
+    }
+
+    pub fn can_buy(&self, value: f64) -> bool {
+        self.money >= value
+    }
+
+    pub fn add_mail(&mut self, mut mail: Mail) {
+        mail.id = self.mails.len();
+        self.mails.push(mail);
+    }
+
+    pub fn fetch_mails(&self) -> &Vec<Mail> {
+        &self.mails
     }
 }
