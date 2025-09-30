9afd182b9588e2e7fab921a9220e150292c30046
Kiyotoko <karl.zschiebsch@gmail.com>
2025-09-30 18:04:38 +0200

    feat: add stock market

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,18 @@
 mod event;
 mod law;
+mod market;
 mod modifier;
-mod player;
 mod shader;
 
 use crate::{
     event::Event,
-    law::{Law, Party},
-    modifier::{ModType, Modifier, Resource},
-    player::Player,
+    law::{Law, Parlament, Party},
+    market::Market,
     shader::{CRT_FRAGMENT_SHADER, CRT_VERTEX_SHADER},
 };
 use macroquad::prelude::*;
-use macroquad::ui::{
-    hash, root_ui,
-    widgets::{Group, Window},
-};
-use std::f64::consts::PI;
+use macroquad::ui::{hash, root_ui, widgets::Window};
+use std::collections::VecDeque;
 
 fn window_conf() -> Conf {
     Conf {
@@ -45,50 +41,46 @@ async fn main() {
     )
     .unwrap();
 
-    let mut player = Player::new();
+    let mut market = Market::new();
 
     let events: Vec<Event> = {
         let serialized = load_string("assets/events.json").await.unwrap();
         serde_json::from_str(&serialized).unwrap()
     };
 
-    let laws: Vec<Law> = {
-        let serialized = load_string("assets/laws.json").await.unwrap();
-        serde_json::from_str(&serialized).unwrap()
+    let mut parlament = {
+        let parties = vec![
+            Party {
+                approval: 0.34,
+                popularity: 0.45,
+                color: RED,
+            },
+            Party {
+                approval: 0.82,
+                popularity: 0.35,
+                color: GREEN,
+            },
+            Party {
+                approval: 0.82,
+                popularity: 0.2,
+                color: BLUE,
+            },
+        ];
+
+        let available_laws: VecDeque<Law> = {
+            let serialized = load_string("assets/laws.json").await.unwrap();
+            serde_json::from_str(&serialized).unwrap()
+        };
+
+        let passed_laws: VecDeque<Law> = VecDeque::new();
+
+        Parlament {
+            parties,
+            available_laws,
+            passed_laws,
+            voting_time: 0.,
+        }
     };
-
-    let parties = vec![
-        Party {
-            approval: 0.34,
-            popularity: 0.45,
-            color: RED,
-        },
-        Party {
-            approval: 0.82,
-            popularity: 0.35,
-            color: GREEN,
-        },
-        Party {
-            approval: 0.82,
-            popularity: 0.2,
-            color: BLUE,
-        },
-    ];
-
-    let stock_market = vec![
-        Vec2::new(0., 10.),
-        Vec2::new(1., 30.),
-        Vec2::new(2., 20.),
-        Vec2::new(3., 50.),
-        Vec2::new(4., 20.),
-        Vec2::new(5., 60.),
-        Vec2::new(5.5, 10.),
-        Vec2::new(5.9, 30.),
-        Vec2::new(6.4, 20.),
-        Vec2::new(6.9, 50.),
-        Vec2::new(7.5, 20.),
-        Vec2::new(9., 60.),
-    ];
 
     loop {
         #[cfg(not(target_arch = "wasm32"))]
@@ -103,91 +95,24 @@ async fn main() {
         gl_use_material(&material);
         clear_background(WHITE);
 
-        Window::new(hash!(), Vec2::new(30., 50.), Vec2::new(200., 180.))
+        Window::new(hash!(), Vec2::new(30., 50.), Vec2::new(200., 220.))
             .label("Stock Market")
             .ui(&mut *root_ui(), |ui| {
-                let mut canvas = ui.canvas();
-                let cursor = canvas.cursor();
-
-                let (mut min_x, mut max_x, mut min_y, mut max_y) = (0., 0., 0., 0.);
-                for marker in &stock_market {
-                    if marker.x < min_x {
-                        min_x = marker.x
-                    } else if marker.x > max_x {
-                        max_x = marker.x
-                    }
-                    if marker.y < min_y {
-                        min_y = marker.y
-                    } else if marker.y > max_y {
-                        max_y = marker.y
-                    }
-                }
-                let (min, max) = (
-                    Vec2::new(min_x, min_y),
-                    Vec2::new(max_x / 200., max_y / 130.),
-                );
-                for i in 0..stock_market.len() - 1 {
-                    let first = stock_market
-                        .get(i)
-                        .expect("expected first stock market item");
-                    let second = stock_market
-                        .get(i + 1)
-                        .expect("expected second stock market item");
-                    canvas.line(
-                        Vec2::new(
-                            cursor.x + (first.x - min.x) / max.x,
-                            cursor.y + 130. - (first.y - min.y) / max.y,
-                        ),
-                        Vec2::new(
-                            cursor.x + (second.x - min.x) / max.x,
-                            cursor.y + 130. - (second.y - min.y) / max.y,
-                        ),
-                        if first.y <= second.y { GREEN } else { RED },
-                    );
-                }
-
-                if ui.button(Vec2::new(10., 140.), "Buy") {}
-                if ui.button(Vec2::new(50., 140.), "Sell") {}
+                market.update();
+                market.draw_on(ui);
             });
 
         Window::new(hash!(), Vec2::new(110., 80.), Vec2::new(400., 400.))
             .label("Parlament")
             .ui(&mut *root_ui(), |ui| {
-                let mut canvas = ui.canvas();
-                let cursor = canvas.cursor();
-
-                const TOTAL_SEATS: f32 = (9 * 4) as f32;
-                // dbg!(&TOTAL_SEATS);
-                let mut placed = 0.;
-                let mut party_num = 0;
-                for arc in 0..9 {
-                    let base = if arc % 2 == 0 { 4 } else { 3 };
-                    for row in 0..base {
-                        let party = parties.get(party_num).expect("expect party exists");
-                        let angle = arc as f32 / 8. * PI as f32;
-                        canvas.rect(
-                            Rect::new(
-                                200. + cursor.x - angle.cos() * 40. * (row + 5 - base) as f32,
-                                200. + cursor.y - angle.sin() * 40. * (row + 5 - base) as f32,
-                                15.0,
-                                15.0,
-                            ),
-                            Color::new(0.2, 0.2, 0.2, 1.0),
-                            party.color,
-                        );
-                        placed += (1.0 / party.popularity) / TOTAL_SEATS;
-                        if placed >= 1. {
-                            placed = 0.;
-                            party_num += 1;
-                        }
-                    }
-                }
+                parlament.update();
+                parlament.draw_on(ui);
             });
 
         Window::new(hash!(), Vec2::new(100., 400.), Vec2::new(400., 200.))
             .label("Laws")
             .ui(&mut *root_ui(), |ui| {
-                for law in &laws {
+                for law in &parlament.available_laws {
                     law.draw_on(ui);
                 }
             });
---
+++ src/market.rs
@@ -0,0 +1,97 @@
+use macroquad::prelude::*;
+use macroquad::ui::Ui;
+use std::collections::VecDeque;
+
+#[derive(Debug)]
+pub struct Market {
+    money: f32,
+    price: f32,
+    stocks: usize,
+    trading_time: f32,
+    history: VecDeque<f32>,
+}
+
+impl Market {
+    pub fn new() -> Market {
+        Market {
+            money: 100.,
+            price: 10.,
+            stocks: 100,
+            trading_time: 0.,
+            history: vec![
+                9., 9.2, 8.9, 8.8, 9.1, 9.2, 9.4, 9.6, 9.8, 10., 9., 9.2, 8.9, 8.8, 9.1, 9.2, 9.4,
+                9.6, 9.8, 10., 9., 9.2, 8.9, 8.8, 9.1, 9.2, 9.4, 9.6, 9.8, 10.,
+            ]
+            .into(),
+        }
+    }
+
+    pub fn draw_on(&mut self, ui: &mut Ui) {
+        let mut canvas = ui.canvas();
+        let cursor = canvas.cursor();
+
+        // Fetch the history of the stock market and
+        // calculate the global minimum and maximum
+        let (mut min, mut max) = (1000., 0.);
+        for marker in &self.history {
+            let value = *marker;
+            if value < min {
+                min = value
+            } else if value > max {
+                max = value
+            }
+        }
+
+        // Draw all stock market lines
+        let markers = self.history.len();
+        for i in 0..markers - 1 {
+            let first = self
+                .history
+                .get(i)
+                .expect("expected first stock market item");
+            let second = self
+                .history
+                .get(i + 1)
+                .expect("expected second stock market item");
+            canvas.line(
+                Vec2::new(
+                    cursor.x + (i as f32 / markers as f32) * 180.,
+                    cursor.y + 130. - (first - min) / (max - min) * 130.,
+                ),
+                Vec2::new(
+                    cursor.x + ((i + 1) as f32 / markers as f32) * 180.,
+                    cursor.y + 130. - (second - min) / (max - min) * 130.,
+                ),
+                if first <= second { GREEN } else { RED },
+            );
+        }
+
+        ui.label(Vec2::new(10., 140.), &format!("Money: {}", self.money));
+        ui.label(Vec2::new(10., 160.), &format!("Price: {}", self.price));
+        ui.label(Vec2::new(10., 180.), &format!("Stocks: {}", self.stocks));
+        if ui.button(Vec2::new(140., 140.), "Buy") {
+            if self.money >= self.price {
+                self.money -= self.price;
+                self.stocks += 1;
+            }
+        }
+        if ui.button(Vec2::new(140., 160.), "Sell") {
+            if self.stocks >= 1 {
+                self.money += self.price;
+                self.stocks -= 1;
+            }
+        }
+    }
+
+    pub fn update(&mut self) {
+        self.trading_time += get_frame_time();
+        if self.trading_time >= 1. {
+            self.history.push_back(self.price);
+            self.price += rand::gen_range(0.0, 2.2) - 1.;
+            self.history
+                .pop_front()
+                .expect("expect history marker exists");
+            self.trading_time -= 1.;
+        }
+    }
+}
