b8ccf42d21d65eefe75f8fd9cdc3a86340bd1cbf
Kiyotoko <karl.zschiebsch@gmail.com>
2025-09-30 15:24:09 +0200

    feat: add parties

--- src/law.rs
+++ src/law.rs
@@ -1,4 +1,12 @@
+use macroquad::prelude::*;
 use serde::Deserialize;
+
+#[derive(Debug)]
+pub struct Party {
+    pub approval: f32,
+    pub popularity: f32,
+    pub color: Color,
+}
 
 #[derive(Debug, Deserialize)]
 pub struct Law {
--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,19 @@ mod modifier;
 mod player;
 mod shader;
 
-use crate::event::Event;
-use crate::modifier::{ModType, Modifier, Resource};
-use crate::player::Player;
-use crate::shader::{CRT_FRAGMENT_SHADER, CRT_VERTEX_SHADER};
+use crate::{
+    event::Event,
+    law::Party,
+    modifier::{ModType, Modifier, Resource},
+    player::Player,
+    shader::{CRT_FRAGMENT_SHADER, CRT_VERTEX_SHADER},
+};
 use macroquad::prelude::*;
 use macroquad::ui::{
-    Skin, hash, root_ui,
+    hash, root_ui,
     widgets::{Group, Window},
 };
+use std::f64::consts::PI;
 
 fn window_conf() -> Conf {
     Conf {
@@ -45,13 +49,23 @@ async fn main() {
     let serialized = load_string("assets/events.json").await.unwrap();
     let events: Vec<Event> = serde_json::from_str(&serialized).unwrap();
 
-    let modifier = Modifier {
-        mod_type: ModType::Constant,
-        resource: Resource::Money,
-        value: 100.,
-    };
-    let serialized = serde_json::to_string(&modifier).unwrap();
-    dbg!(serialized);
+    let mut parties = vec![
+        Party {
+            approval: 0.34,
+            popularity: 0.45,
+            color: RED,
+        },
+        Party {
+            approval: 0.82,
+            popularity: 0.35,
+            color: GREEN,
+        },
+        Party {
+            approval: 0.82,
+            popularity: 0.2,
+            color: BLUE,
+        },
+    ];
 
     loop {
         #[cfg(not(target_arch = "wasm32"))]
@@ -65,6 +79,40 @@ async fn main() {
         });
         gl_use_material(&material);
         clear_background(WHITE);
+
+        Window::new(hash!(), Vec2::new(30., 80.), Vec2::new(400., 400.))
+            .label("Parlament")
+            .ui(&mut *root_ui(), |ui| {
+                let mut canvas = ui.canvas();
+                let cursor = canvas.cursor();
+
+                const TOTAL_SEATS: f32 = (9 * 4) as f32;
+                // dbg!(&TOTAL_SEATS);
+                let mut placed = 0.;
+                let mut party_num = 0;
+                for arc in 0..9 {
+                    let base = if arc % 2 == 0 { 4 } else { 3 };
+                    for row in 0..base {
+                        let party = parties.get(party_num).expect("expect party exists");
+                        let angle = arc as f32 / 8. * PI as f32;
+                        canvas.rect(
+                            Rect::new(
+                                200. + cursor.x - angle.cos() * 40. * (row + 5 - base) as f32,
+                                200. + cursor.y - angle.sin() * 40. * (row + 5 - base) as f32,
+                                15.0,
+                                15.0,
+                            ),
+                            Color::new(0.2, 0.2, 0.2, 1.0),
+                            party.color,
+                        );
+                        placed += (1.0 / party.popularity) / TOTAL_SEATS;
+                        if placed >= 1. {
+                            placed = 0.;
+                            party_num += 1;
+                        }
+                    }
+                }
+            });
 
         Window::new(hash!(), Vec2::new(50., 250.), Vec2::new(320., 200.))
             .label("Shop")
